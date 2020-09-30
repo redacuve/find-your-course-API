4468e746d54035cfb03a89f025f0fd490f137fca
Rey David Cuevas <cuevas.vela@gmail.com>
2020-09-30 15:11:43 -0500

    added the gem bcrypt for a secure password in the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
+  # secure password
+  has_secure_password
+
   # associations
   has_many :favorites, class_name: 'Favourite', foreign_key: 'user_id', dependent: :destroy
   has_many :ratings, class_name: 'Rating', foreign_key: 'user_id', dependent: :destroy
