2ae0e55d3160cc35a7c2442ac1d44d33508d8f71
Rey David Cuevas <cuevas.vela@gmail.com>
2020-10-01 17:32:06 -0500

    update with the authorize request private method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,14 @@
 class ApplicationController < ActionController::API
+  attr_reader :current_user
+
   include Response
   include ExceptionHandler
+
+  before_action :authorize_request
+
+  private
+
+  def authorize_request
+    @current_user = (AuthorizeApiRequest.new(request.headers).call)[:user]
+  end
 end
