ad99083f33cf48d2e46b7e88c5b1790f96736c47
Rey David Cuevas <cuevas.vela@gmail.com>
2020-10-06 15:37:26 -0500

    added a new field named username to show it on the response

--- app/controllers/authentication_controller.rb
+++ app/controllers/authentication_controller.rb
@@ -3,7 +3,8 @@ class AuthenticationController < ApplicationController
 
   def authenticate
     auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
-    json_response(auth_token: auth_token)
+    user = User.find_by(email: auth_params[:email])
+    json_response({ auth_token: auth_token, username: user.username })
   end
 
   private
